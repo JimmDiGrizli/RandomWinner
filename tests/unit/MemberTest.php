e18098482d4d626be295445319fe899e544f3f4a
Alexander.Getmansky <gaa@szrcai.ru>
2015-02-19 16:47:47 +0300

    Updated member tests

--- tests/unit/MemberTest.php
+++ tests/unit/MemberTest.php
@@ -4,6 +4,8 @@ use GetSky\RandomWinner\Member;
 
 class MemberTest extends PHPUnit_Framework_TestCase
 {
+    const TEST_CLASS = 'GetSky\RandomWinner\Member';
+
     /**
      * @dataProvider providerForMember
      * @param $object
@@ -13,8 +15,46 @@ class MemberTest extends PHPUnit_Framework_TestCase
     {
         $member = new Member($object, $chance);
 
-        $this->assertSame($member->getObject(), $object);
-        $this->assertSame($member->getChance(), (int)$chance);
+        $this->assertAttributeEquals($object, 'object', $member);
+        $this->assertAttributeEquals((int)$chance, 'chance', $member);
+    }
+
+    /**
+     * @dataProvider providerForMember
+     * @param $object
+     * @param $chance
+     */
+    public function testGetChance($object, $chance)
+    {
+        $member = $this->member('chance', $chance);
+        $this->assertSame($chance, $member->getChance());
+    }
+
+    /**
+     * @dataProvider providerForMember
+     * @param $object
+     * @param $chance
+     */
+    public function testGetObject($object, $chance)
+    {
+        $member = $this->member('object', $object);
+        $this->assertSame($object, $member->getObject());
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return Member
+     */
+    protected function member($name, $value)
+    {
+        /** @var GetSky\RandomWinner\Member $member */
+        $member = $this->getMockBuilder($this::TEST_CLASS)->disableOriginalConstructor()->setMethods(null)->getMock();
+        $aMember = new ReflectionProperty($this::TEST_CLASS, $name);
+        $aMember->setAccessible(true);
+        $aMember->setValue($member, $value);
+        return $member;
+
     }
 
     /**

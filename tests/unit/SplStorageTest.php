eac903ae5e9bcec6313279170f8801db0004408a
Alexander.Getmansky <gaa@szrcai.ru>
2015-02-26 17:17:58 +0300

    Update getUpperLimit test

--- tests/unit/SplStorageTest.php
+++ tests/unit/SplStorageTest.php
@@ -10,18 +10,21 @@ class SplStorageTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeInstanceOf('SplObjectStorage', 'members', $splStorage);
     }
 
-    public function testGetUpperLimit()
+    /**
+     *  @dataProvider getUpperData
+     * @param $storage
+     * @param $max
+     */
+    public function testGetUpperLimit($storage, $max)
     {
         $splStorage = new SplStorage();
-/**
-        $upperLimit = new ReflectionProperty('GetSky\RandomWinner\SplStorage', 'upperLimit');
-        $upperLimit->setAccessible(true);
+        $this->assertSame(0, $splStorage->getUpperLimit());
 
-        $upperLimit->setValue($splStorage, 18);
-        $this->assertSame(18, $splStorage->getUpperLimit());
-        $upperLimit->setValue($splStorage, 45);
-        $this->assertSame(45, $splStorage->getUpperLimit());
- **/
+        $members = new ReflectionProperty('GetSky\RandomWinner\SplStorage', 'members');
+        $members->setAccessible(true);
+        $members->setValue($splStorage, $storage);
+
+        $this->assertSame($max, $splStorage->getUpperLimit());
     }
 
     public function testGetAll()
@@ -34,20 +37,56 @@ class SplStorageTest extends PHPUnit_Framework_TestCase
         $this->assertSame($members->getValue($splStorage), $splStorage->getAll());
     }
 
-    public function testGetRange()
+    /**
+     *  @dataProvider getRangeData
+     * @param $range
+     */
+    public function testGetRange($range)
     {
-        $member = $this->getMock('GetSky\RandomWinner\MemberInterface');
-        $splStorage = $this->getSplStorage($member, [0,50], 'offsetGet');
+        $member = $this->getMember('test', null);
+        $splStorage = $this->getSplStorage($member, $range, 'offsetGet');
 
-        $this->assertSame([0, 50], $splStorage->getRange($member));
+        $this->assertSame($range, $splStorage->getRange($member));
     }
 
     public function testContains()
     {
-        $member = $this->getMock('GetSky\RandomWinner\MemberInterface');
+        $member = $this->getMember('test', 10);
         $splStorage = $this->getSplStorage($member, true, 'contains');
 
         $this->assertSame(true, $splStorage->contains($member));
+    }
+
+    public function getRangeData() {
+        return [
+            [0, 50],
+            [10,100],
+            [0,35]
+        ];
+    }
+
+    public function getUpperData()
+    {
+        $spl1 = new SplObjectStorage();
+        $spl1->attach($this->getMember('test', 45));
+
+        $spl2 = new SplObjectStorage();
+        $spl2->attach(($this->getMember('test', 15)));
+        $spl2->attach(($this->getMember('test', 5)));
+        $spl2->attach(($this->getMember('test', 1)));
+
+        $spl3 = new SplObjectStorage();
+        $spl3->attach(($this->getMember('test', 1)));
+        $spl3->attach(($this->getMember('test', 9)));
+        $spl3->attach(($this->getMember('test', 60)));
+        $spl3->attach(($this->getMember('test', 5)));
+
+        return [
+            [$spl1, 45],
+            [new SplObjectStorage(), 0],
+            [$spl2, 15],
+            [$spl3, 60],
+        ];
     }
 
     protected function getSplStorage($member, $value, $method)
@@ -64,5 +103,27 @@ class SplStorageTest extends PHPUnit_Framework_TestCase
         $members->setAccessible(true);
         $members->setValue($splStorage, $splObjectStorage);
         return $splStorage;
+    }
+
+    /**
+     * @return GetSky\RandomWinner\Member
+     */
+    protected function getMember($object, $chance)
+    {
+        $mock = $this->getMockBuilder('GetSky\RandomWinner\MemberInterface')
+            ->setMethods([])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock
+            ->expects($this->never())
+            ->method('getObject')
+            ->will($this->returnValue($object));
+        $mock
+            ->expects($this->any())
+            ->method('getChance')
+            ->will($this->returnValue((int)$chance));
+
+        return $mock;
     }
 }
